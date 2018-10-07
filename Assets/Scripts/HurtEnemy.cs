0837afb650636ec01505da7eea3dfe0a11bb8cf1
ItzStriker <itzstriker@users.noreply.github.com>
2018-10-06 22:18:23 -0300

    Initial commit

---
+++ Assets/Scripts/HurtEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtEnemy : MonoBehaviour {
+
+    public int damageToGive;
+    public GameObject damageBurst;
+    public Transform hitPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            // Dano recebido pelo inimigo chamando o script da vida
+            other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToGive);
+            //Animação de dano da espada
+            Instantiate(damageBurst, hitPoint.position, hitPoint.rotation);       }
+    }
+}
---
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour {
+
+    public int vidaMaxima;
+    public int vidaAtual;
+
+	// Use this for initialization
+	void Start () {
+        vidaAtual = vidaMaxima;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(vidaAtual <= 0)
+        {
+            gameObject.SetActive(false);
+
+            
+        }
+		
+	}
+
+    public void HurtPlayer(int damageToGive)
+    {
+        vidaAtual -= damageToGive;
+    }
+    public void SetMaxHealth()
+    {
+        vidaAtual = vidaMaxima;
+
+    }
+}
